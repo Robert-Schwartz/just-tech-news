ac26c838c9168a3227dc2ad80813aa08b191ae33
Robert Schwartz <bschwartz23@gmail.com>
2021-05-31 22:35:12 -0700

    Create the Database Connection and Set Up Environment Variables

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// call the Sequelize connection constructor function
+// ==================================================
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+//require dotenv to hide password in an environment file
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for username and password from the .env file
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
+
+/*
+All we're doing here is importing the base Sequelize class and using it to create a new connection to the database. The new Sequelize() function accepts the database name, MySQL username, and MySQL password (respectively) as parameters, then we also pass configuration settings. Once we're done, we simply export the connection
+*/
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- Create and Connect to the Database
+DROP DATABASE IF EXISTS just_tech_news_db;
+
+CREATE DATABASE just_tech_news_db;

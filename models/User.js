f313e80de2454bf4a97337366950f4bb8c57529c
Robert Schwartz <bschwartz23@gmail.com>
2021-05-31 23:22:59 -0700

    Create API Routes for the User Model

--- models/User.js
+++ models/User.js
@@ -1,67 +1,44 @@
-// Model class is what we create our own models from using 'Extends' keyword so User inherits all of the functionality the Model class has.
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// create our User model
 class User extends Model { }
-/*
-Once we create the User class, we use the.init() method to initialize the model's data and configuration, passing in two objects as arguments. The first object will define the columns and data types for those columns. The second object it accepts configures certain options for the table.
-*/
+// See model-example file for review
 
-// define table columns and configuration
 User.init(
-    {// TABLE COLUMN DEFINITIONS GO HERE AS THE FIRST ARGUMENT
-        // define an id column
+    {// TABLE COLUMN DEFINITIONS GO HERE
         id: {
-            // use the special Sequelize DataTypes object provide what type of data it is
             type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
             allowNull: false,
-            // instruct that this is the Primary Key
             primaryKey: true,
-            // turn on auto increment
             autoIncrement: true
         },
-        // define a username column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
-        // define an email column
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            // there cannot be any duplicate email values in this table
             unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
             validate: {
                 isEmail: true
             }
         },
-        // define a password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                // this means the password must be at least four characters long
                 len: [4]
             }
         }
     },
-    { // TABLE CONFIGURATION OPTIONS GO HERE AS SECOND ARGUMENT (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-        // pass in our imported sequelize connection (the direct connection to our database)
+    { //TABLE CONFIGURATION OPTIONS GO HERE
         sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
-        // make it so our model name stays lowercase in the database
         modelName: 'user'
     }
 );
 
 module.exports = User;
-//Lastly, export the newly created model as (User) so we can use it in other parts of the app

358765c8d380f2fad83c17b0164997219831f426
Robert Schwartz <bschwartz23@gmail.com>
2021-06-11 00:12:16 -0700

    Create a Session on the Back End

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,13 +4,18 @@ const { Post, User, Comment, Vote } = require('../models');
 
 // get login
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log('======================');
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
--- server.js
+++ server.js
@@ -10,11 +10,22 @@ The other thing to notice is the use of {force: false} in the .sync() method. Th
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
@@ -24,6 +35,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
 
 app.use(require('./controllers/'));
 

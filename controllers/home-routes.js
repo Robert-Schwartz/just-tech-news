1876d419a847ace499eb9bae42bb65e9760fb5a4
Robert Schwartz <bschwartz23@gmail.com>
2021-06-10 22:47:37 -0700

    14.2.3 Create a Login Page view and route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
+// get login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');

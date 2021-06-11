518713c68abce77893d2191fbd21d65a93d3483d
Robert Schwartz <bschwartz23@gmail.com>
2021-06-11 00:18:59 -0700

    Add Logic to Destroy the Session during log out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -142,4 +142,17 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+// destroy the session on Log Out
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+
 module.exports = router;

8c8f8517eb508b05e24685fecb978e4086444209
Robert Schwartz <bschwartz23@gmail.com>
2021-06-12 17:22:00 -0700

    add login.js file to public

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ The other thing to notice is the use of {force: false} in the .sync() method. Th
 
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -24,8 +28,7 @@ const sess = {
     })
 };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -35,8 +38,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(session(sess));
 
 app.use(require('./controllers/'));
 

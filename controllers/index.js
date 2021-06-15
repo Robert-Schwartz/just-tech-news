0a3f061ab5f5409b9b8c667ed9e78c187f7cdc6a
Robert Schwartz <bschwartz23@gmail.com>
2021-06-14 22:02:27 -0700

    Protect Routes with Middleware

--- controllers/index.js
+++ controllers/index.js
@@ -10,12 +10,13 @@ Make sure that the server.js file you created is at the root of the entire proje
 */
 
 const router = require('express').Router();
-
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
 
